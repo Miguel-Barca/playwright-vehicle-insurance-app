c24ee8faf83de893b492bbd1df249644fe7e8171
Migeul-Barca <fugu4u@gmail.com>
2024-02-24 10:55:46 +0000

    cleaned code duplication

--- tests/page-objects/homepage.spec.ts
+++ tests/page-objects/homepage.spec.ts
@@ -1,28 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page, TestInfo } from '@playwright/test';
 import { tricentisLogoLocator, automobileLocator, camperLocator, truckLocator, motorcycleLocator, selectedInsuranceLocator } from '../locators/pageLocators.ts';
 
-test.describe('Homepage', () => {
+test.describe('Homepage - Selecting Categories', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('');
+        await page.goto('/');
         // Assert visibility of common elements
         await expect(truckLocator(page)).toBeVisible();
         await expect(automobileLocator(page)).toBeVisible();
         await expect(motorcycleLocator(page)).toBeVisible();
         await expect(camperLocator(page)).toBeVisible();
-    });
-
-    test('Homepage', async ({ page }) => {
-        // Verify page title
-        await expect(page).toHaveTitle("Tricentis Vehicle Insurance");
-    });
-
-    test('Select Category', async ({ page }, categoryName) => {
-        // Click on the category and verify selected insurance
-        const categoryLocator = getCategoryLocator(categoryName);
-        await categoryLocator(page).click();
-        await expect(selectedInsuranceLocator(page)).toContainText(`${categoryName} Insurance`);
-        await tricentisLogoLocator(page).click();
-        await expect(page.locator('#app_sub_title')).toContainText('Vehicle Insurance Application');
     });
 
     const categories = ['Automobile', 'Truck', 'Motorcycle', 'Camper'];
@@ -32,21 +18,28 @@ test.describe('Homepage', () => {
                 type: 'summary',
                 description: `Selecting ${category} Category and going back to the Homepage`,
             });
-            
+
             await selectCategory(page, category);
         });
     });
 });
 
-async function selectCategory(page, category) {
+
+async function validateHomepage(page: Page) {
+    // Verify page title
+    await expect(page).toHaveTitle("Tricentis Vehicle Insurance");
+}
+
+async function selectCategory(page: Page, category: string) {
     const categoryLocator = getCategoryLocator(category);
     await categoryLocator(page).click();
     await expect(selectedInsuranceLocator(page)).toContainText(`${category} Insurance`);
     await tricentisLogoLocator(page).click();
+    await validateHomepage(page);
     await expect(page.locator('#app_sub_title')).toContainText('Vehicle Insurance Application');
 }
 
-function getCategoryLocator(category) {
+function getCategoryLocator(category: string | TestInfo) {
     switch (category) {
         case 'Automobile':
             return automobileLocator;
