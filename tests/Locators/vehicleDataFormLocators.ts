20a34962df0614a2b4b1e49612dde598ccddda0e
Migeul-Barca <fugu4u@gmail.com>
2024-03-02 11:29:07 +0000

    Created Locators for Vehicle form and "Automobile" PO

---
+++ tests/Locators/vehicleDataFormLocators.ts
@@ -0,0 +1,31 @@
+export const selectedInsurance = (page) => (page).locator('span[id=selectedinsurance]'); //Vehicle Category chosen by the user
+export const MAKE_CAR = (page) => (page).locator('select[id=make]'); 
+export const ENGINE_PERFORMANCE = (page) => (page).locator('input[id=engineperformance]'); 
+export const MANUFACTURE_DATE = (page) => (page).locator('input[id=dateofmanufacture]'); 
+export const NUMBER_OF_SEATS = (page) => (page).locator('select[id=numberofseats]'); 
+export const FUEL_TYPE = (page) => (page).locator('select[id=fuel]'); 
+export const PAYLOAD = (page) => (page).locator('input[id=payload]'); 
+export const TOTAL_WEIGHT = (page) => (page).locator('input[id=totalweight]'); 
+export const LIST_PRICE = (page) => (page).locator('input[id=listprice]'); 
+export const LICENSE_PLATE = (page) => (page).locator('input[id=licenseplatenumber]'); 
+export const ANNUAL_MILEAGE = (page) => (page).locator('input[id=annualmileage]'); 
+export const NEXT_PAGE_BUTTON = 'nextenterinsurantdata '; 
+export const MOTORCYCLE_MODEL = (page) => (page).locator('select[id=model]'); 
+export const CYLINDER_CAPACITY = (page) => (page).locator('input[id=cylindercapacity]'); 
+export const NUMBER_OF_SEATS_MOTORCYCLE = (page) => (page).locator('select[id=numberofseatsmotorcycle]'); 
+export const RIGHT_HAND_YES_RADIO = (page) => (page).locator('*[@id="insurance-form"]//div[5]/p/label[1]/span'); 
+export const RIGHT_HAND_NO_RADIO = (page) => (page).locator('*[@id="insurance-form"]//div[5]/p/label[2]/span'); 
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
---
+++ tests/page-objects/automobileForms.ts
@@ -0,0 +1,6 @@
+import * as vehicleDataFormLocators from '../locators/vehicleDataFormLocators';
+import { Page, TestInfo } from '@playwright/test';
+
+export async function fillVehicleDataForm(page: Page, category: string) {
+    //TODO
+}
